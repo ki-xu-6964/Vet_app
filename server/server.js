c458bcbc53c93e9d954fcc2cf0ca570d52d98ea8
kittykat6964 <kitty@zenbly.io>
2023-08-26 16:46:37 +1000

    added cors header

--- server/server.js
+++ server/server.js
@@ -40,6 +40,15 @@ app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳  merugh')
 })
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // You can also add other CORS headers here
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+
 
 router.get('/home', async (req, res) => {
   const pets= await Pets.find()
