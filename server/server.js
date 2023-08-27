2a37599760ab95b788aa73a3ad2ce0815ae24420
kittykat6964 <kitty@zenbly.io>
2023-08-27 20:45:03 +1000

    added post route

--- server/server.js
+++ server/server.js
@@ -51,6 +51,22 @@ app.use((req, res, next) => {
   next();
 });
 
+app.post('/', async (req, res) => {
+  const newPettie = new Pets({ 
+    name: req.body.name,
+    age: req.body.age,
+    breed: req.body.breed,
+    
+  })
+
+  try {
+  
+    const finalPettie = await newPettie.save()
+    res.status(201).json(finalPettie)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
 
 
 router.get('/:id', async (req, res) => {
